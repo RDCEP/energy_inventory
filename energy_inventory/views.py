c206b5106c571ba724166ec1e50db1f00775410f
njmattes <matteson@obstructures.org>
2016-07-08 11:17:22 -0500

    Add routes for maps

--- energy_inventory/views.py
+++ energy_inventory/views.py
@@ -15,3 +15,35 @@ def index():
         'index.html',
         variable_from_python=r,
     )
+
+
+@mod.route('/coal_mines')
+def coal_mines():
+    return render_template('coal_mines.html')
+
+
+@mod.route('/oil_gas_extraction')
+def oil_gas_extraction():
+    return render_template('oil_gas_extraction.html')
+
+
+@mod.route('/oil_gas_storage')
+def oil_gas_storage():
+    return render_template('oil_gas_storage.html')
+
+
+@mod.route('/power_plants')
+def power_plants():
+    return render_template('power_plants.html')
+
+
+@mod.route('/petroleum_crude_pipelines')
+def petroleum_crude_pipelines():
+    return render_template('petroleum_crude_pipelines.html')
+
+
+@mod.route('/natural_gas_pipelines')
+def natural_gas_pipelines():
+    return render_template('natural_gas_pipelines.html')
+
+
